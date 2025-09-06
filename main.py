5d831d0c73616339a3b238da60ad8415e307dcc9
Daniel Altamirano <12179811+Daniel-Altamirano@users.noreply.github.com>
2025-09-05 17:02:01 -0700

    added Player object and actions (#2)
    * completed Draw Player
    
    * completed Moving Around
    
    * compled Moving
    
    * completed Groups

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ from constants import (
     ASTEROID_KINDS,
     ASTEROID_SPAWN_RATE,
     ASTEROID_MAX_RADIUS,
+    PLAYER_RADIUS,
 )
+from circleshape import CircleShape
+from player import Player
+
 
 def main():
     print("Starting Asteroids!")
@@ -16,6 +20,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    player = Player(
+        x=SCREEN_WIDTH / 2, 
+        y=SCREEN_HEIGHT / 2,
+    )
+
 
     running = True
     while running:
@@ -23,11 +35,18 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        BLACK = (0, 0, 0)
+        screen.fill(BLACK)
+        # player.update(dt)
+        # player.draw(screen=screen)
+        updateable.update(dt)
+        for item in drawable:
+            item.draw(screen=screen)
+
         pygame.display.flip()
         conversion_factor_milliseconds_to_seconds = 1 / 1000
-        seconds_since_last_tick = clock.tick(60) * conversion_factor_milliseconds_to_seconds
-        dt += seconds_since_last_tick
+        time_since_last_tick_seconds = clock.tick(60) * conversion_factor_milliseconds_to_seconds
+        dt += time_since_last_tick_seconds
         
 
 if __name__ == "__main__":
